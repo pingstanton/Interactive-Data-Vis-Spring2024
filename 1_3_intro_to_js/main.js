e287e63f76c45d8715e3c0568156f204a258c971
pingstanton <108839493+pingstanton@users.noreply.github.com>
2024-02-15 22:28:37 -0500

    Update main.js

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,43 +1,26 @@
-body {
-	margin: 0; padding: 10px 15px 20px 15px; 
-	background-color: #e0e0e0);
-	font: normal 1.0em/1.2em arial, helvetica, sans-serif;
-	color: #000;
-	background-color: #fff;
+// set initial variables...
+let clickCount = document.getElementById('numClicks');
+let clickButton = document.getElementById('clickButton');
+let count = 0;
+
+// conditional display of count in numClicks <div> ...
+function updateCount() {
+	if (count === 0) {
+		clickCount.textContent = "The button has not yet been clicked. Count is currently 0.";
+		}
+    else if (count === 1) {
+		clickCount.textContent = "The button has been clicked once. Count is currently 1.";
+		}
+	else if (count > 1) {
+		clickCount.textContent = "The button has now been clicked " + count + " times.";
+		}
 	}
 
-a:link {color: #060; text-decoration: none; border-bottom: 0px solid #fff;}
-a:visited {color: #060; text-decoration: none; border-bottom: 0px solid #fff;}
-a:hover  {color: #0c9; text-decoration: none; border-bottom: 0px solid #CC6633;}
-a:active {color: #060; text-decoration: none; border-bottom: 0px solid #fff;}
-
-h1 {margin: 0 0 0.8em 0; padding: 0; font: bold 1.6em/1.0em arial, helvetica, sans-serif; }
-h2 {margin: 0 0 0.8em 0; padding: 0; font: bold 1.4em/1.0em arial, helvetica, sans-serif; }
-h3 {margin: 0 0 0.8em 0; padding: 0; font: bold 1.2em/1.0em arial, helvetica, sans-serif; }
-h4 {margin: 0 0 0.8em 0; padding: 0; font: bold 1.0em/1.0em arial, helvetica, sans-serif; }
-
-.ath {margin: 0.4em 0 2em 0; font: bold 0.8em/1.6em arial, helvetica, sans-serif;}
-
-#numClicks {margin: 0 0 20px 0; padding: 0;
-	font: normal 1.2em/1.0em arial, helvetica, sans-serif;
-	color: #096;
-	}
-
-.subbtn {margin: 0 20px; padding: 7px 30px 5px 30px; 
-	border-radius: 9px;
-	letter-spacing: 3px;
-	font: bold 1.3em/1.0em arial, helvetica, sans-serif; 
-	color: #009966; background-color: #fff;
-	border: 2px solid #fff;		
-	-webkit-box-shadow: 3px 3px 15px 5px rgba(0,0,0,0.25); 
-	box-shadow: 3px 3px 15px 5px rgba(0,0,0,0.25);	
-	}
-
-.subbtn:hover {
-	transition: 0.9s;
-	border: 2px solid #009966;		
-	background-color: #009966; color: #fff;
-	-webkit-box-shadow: 3px 3px 15px 5px rgba(0,0,0,0.50); 
-	box-shadow: 3px 3px 15px 5px rgba(0,0,0,0.50);
+// increase count function...
+function handleClick() {
+	count = count + 1;
+	updateCount();
 }
-
+    
+// click event listener...
+subbtn.addEventListener('click', handleClick);
