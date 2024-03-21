31a7d7dbb17dda9aec5b9ab524cc2008928e188d
pingstanton <108839493+pingstanton@users.noreply.github.com>
2024-03-21 08:55:28 -0400

    Update main.js

--- 2_3_time_series/main.js
+++ 2_3_time_series/main.js
@@ -1,4 +1,32 @@
- /* CONSTANTS AND GLOBALS */
+/* Section 2 | Tutorial 3 | Time Series
+Tutorial Assignment, see:
+https://github.com/InteractiveDataVis/Interactive-Data-Vis-Spring2024/blob/main/2_3_time_series/README.md
+*/
+
+/* DATA 73200 - INTERACTIVE DATA VISUALIZATION
+Instructor: Eleanor Frymire
+
+CUNY GRADUATE CENTER | 365 5th Ave, New York, NY 10016
+
+MATTHEW STANTON | mstanton@gradcenter.cuny.edu | pingstanton@gmail.com
+https://github.com/pingstanton/ | http://chimaboo.com/coursework/DATA73200
+
+*/
+
+/* *** KNOWN ISSUES *** */
+
+/* ERRORS IN CONSOLE?
+"Error: <text> attribute x: Expected length, "function axis(co..."."
+Keeps listing itself in Console but (a.) it doesn't appear to be
+breaking anything visible in the final display, and (b.) Google /
+Stack Overflow suggest it may be a "NaN" error (calling an array
+instead of an integer), but I'm not sure... */
+
+/* NO AXIS LABELS?
+I cannot figure out why none of my visualizations are displaying
+label text left (y) or below (x)... */
+
+/* CONSTANTS AND GLOBALS */
  	const width = window.innerWidth * 0.95,
  	height = window.innerHeight * 0.8,
  	margin = { top: 5, bottom: 20, left: 0, right: 60 }
@@ -10,14 +38,14 @@
 	d3.csv('../data/jamesbond.csv', d => {
 		return {
 		Movie: d.Movie, /* keeps as string */
-		ReleaseYear: new Date(+d.ReleaseYear, 0, 1), /* reformats to year */
+		ReleaseYear: new Date(+d.ReleaseYear, 0, 1), /* reformats to year as time */
 		Actor: d.Actor, /* keeps as string */
 		LastName: d.LastName, /* keeps as string */
 		ActorAge: +d.ActorAge, /* reformats to number */
-		BondGirl: d.BondGirl,
-		BondGirlActor: d.BondGirlActor,
-		BondGirlAge: +d.BondGirlAge,
-		BondLabel: d.BondLabel
+		/* BondLabel manually created in the .csv to make customization 
+		easier;	was originally calculated in the D3 code but length of
+		"On Her Majesty's Secret Service" required shortening. */
+		BondLabel: d.BondLabel 
 			}
 		}).then(data => {
 			console.log('data :>> ', data);
@@ -30,14 +58,16 @@
 
 /* SCALES | domain for source, range for display */
 	const xScale = d3.scaleTime()
-		.domain(d3.extent(data, d => d.ReleaseYear))
-		.range([margin.right, width - margin.left])
+		.domain(d3.extent(data, d => d.ReleaseYear)) /* for input */
+		.range([margin.right, width - margin.left]) /* for output */
 
 	const yScale = d3.scaleLinear()
-	 	.domain([0, d3.max(data, d => d.ActorAge+5)]) // Ensure the domain starts from zero
-	 	.range([height - margin.bottom, margin.top]);
+	 	.domain([0, d3.max(data, d => d.ActorAge+5)]) /* changed to ensure the domain starts from zero */
+	 	.range([height - margin.bottom, margin.top]);  /* for output */
 
 /* CREATE SVG ELEMENT */
+/* named "svgJB" because originally there was going to be a
+second area stacked over the first, or so I thought... */
 	const svgJB = d3.select("#container")
 		.append("svg")
 		.attr("width", width + 10)
@@ -46,7 +76,7 @@
 /* BUILD AND CALL AXES */
 
 	const xAxis = d3.axisBottom(xScale)
-		.ticks(12)
+		.ticks(12) /* displays at every five year mark */
 
 	const xAxisGroup = svgJB.append("g")
 		.attr("class", "xAxis")
@@ -85,16 +115,17 @@
 		.y1(d => yScale(d.ActorAge)); /* top of the area (y-value when ActorAge is maximum) */
 
 /* DRAW AREA */
+/* bitmap background images added via CSS; original plan adding them as
+pattern fill to the display area looked way too busy and hard to read. */
 
 	svgJB.selectAll(".area")
 		.data(data)
 		.join("path")
 		.attr("class", "area")
-		.attr("fill", "#cc0000")
-
+		.attr("fill", "#cc0000") /* slightly darker red */
 		.attr("d", (areaGenJB(data)));
 
-	svgJB.selectAll(".actor-label")
+	svgJB.selectAll(".actor-label") /* actor and movie name per data point */
 		.data(data)
 		.join("text")
 		.attr("class", "actor-label")
@@ -108,16 +139,15 @@
 		.style("font-family", "arial") /* font face */
 		.style("font-weight", "bold") /* font weight */
 		.style("fill", "#ffffff") /* white text */
-		.attr("opacity", 0.9) // Provide opacity as a number
+		.attr("opacity", 0.9) /* slightly transparent */
 		.each(function(d) { 	  
-		/* calculate the x and y offsets for rotation */
+		/* calculate the x and y offsets for rotation, adjusted to fit */
 	    const xOffset = xScale(d.ReleaseYear)+5;
 		const yOffset = yScale(d.ActorAge);
 	    /* apply rotation transformation to each label individually */
 		d3.select(this).attr("transform", `translate(${xOffset},${yOffset}) rotate(90)`);
 		})
-		.html(d => d.BondLabel); // Display ActorAge as the label text
-
+		.html(d => d.BondLabel); /* display BondLabel as the label text */
 
 /* ADDING TABLE TO VIEW RAW SOURCE DATA ON PAGE */
 
